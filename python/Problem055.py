a9b043fbdab0fa18128c21962ddeac19d82ce615
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2010-07-12 13:09:05 -0500

    Adding Python Problem 55.

---
+++ python/Problem055.py
@@ -0,0 +1,86 @@
+#  Copyright (C) 2010  Alexander Gude - alex.public.account+ProjectEulerSolutions@gmail.com
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  The most recent version of this program is avaible at:
+#  http://github.com/Falcorian/Project-Euler-Solutions
+
+import time
+from optparse import OptionParser
+"""
+If we take 47, reverse and add, 47 + 74 = 121, which is palindromic.
+
+Not all numbers produce palindromes so quickly. For example,
+
+349 + 943 = 1292,
+1292 + 2921 = 4213
+4213 + 3124 = 7337
+
+That is, 349 took three iterations to arrive at a palindrome.
+
+Although no one has proved it yet, it is thought that some numbers, like 196, never produce a palindrome. A number that never forms a palindrome through the reverse and add process is called a Lychrel number. Due to the theoretical nature of these numbers, and for the purpose of this problem, we shall assume that a number is Lychrel until proven otherwise. In addition you are given that for every number below ten-thousand, it will either (i) become a palindrome in less than fifty iterations, or, (ii) no one, with all the computing power that exists, has managed so far to map it to a palindrome. In fact, 10677 is the first number to be shown to require over fifty iterations before producing a palindrome: 4668731596684224866951378664 (53 iterations, 28-digits).
+
+Surprisingly, there are palindromic numbers that are themselves Lychrel numbers; the first example is 4994.
+
+How many Lychrel numbers are there below ten-thousand?
+
+NOTE: Wording was modified slightly on 24 April 2007 to emphasise the theoretical nature of Lychrel numbers.
+"""
+# Optparse setup
+usage = "usage: %prog [OPTIONS] -n number"
+parser = OptionParser(usage=usage)
+parser.add_option("-n", "--max", action="store", type="int", dest="max", default=10000, help="the bound on the largest number to check")
+
+(options, args) = parser.parse_args()
+
+# Functions
+def returnReversedInt(num):
+    """ For an int XYZ, returns ZYX """
+    return int(str(num)[::-1])
+
+def isPalindromic(num):
+    """ Returns True/False after checking if num is palindromic """
+    num = str(num)
+
+    for i in range(len(num) / 2):
+        if int(num[i]) != int(num[-(i+1)]):
+            return False
+    
+    return True
+
+def isLychrel(num):
+    """ Returns false if a not a Lychrel number, True if this can't be shown in 50 tries """
+    tries = 0
+    while tries < 50:
+        newNum = num + returnReversedInt(num)
+        if isPalindromic(newNum):
+            return False
+        else:
+            tries += 1
+            num = newNum
+    return True
+
+# Solution
+s = time.time()
+
+max = options.max
+
+lycherels = []
+for i in range(max):
+    if isLychrel(i):
+        #print i
+        lycherels.append(i)
+
+print len(lycherels),'in',time.time()-s,'secs' 
