dc172c5a84429359877619db2e02dd2ceb76b26b
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2010-07-13 16:50:20 -0500

    Adding C++ 14. Also tweaking Python 14

--- python/Problem014.py
+++ python/Problem014.py
@@ -33,7 +33,6 @@ It can be seen that this sequence (starting at 13 and finishing at 1) contains 1
 Which starting number, under one million, produces the longest chain?
 
 NOTE: Once the chain starts the terms are allowed to go above one million.
-
 """
 # Optparse setup
 usage = "usage: %prog [OPTIONS] -n number"
