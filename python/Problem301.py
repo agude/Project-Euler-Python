3f35d0f88428cf99cc6334f40bc0a16519cd5a65
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2011-06-11 17:14:05 +0200

    Python Probelm 301 commited and works.

---
+++ python/Problem301.py
@@ -0,0 +1,72 @@
+#  Copyright (C) 2011  Alexander Gude - alex.public.account+ProjectEulerSolutions@gmail.com
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  The most recent version of this program is avaible at:
+#  http://github.com/Falcorian/Project-Euler-Solutions
+
+import time
+from optparse import OptionParser
+"""
+Nim is a game played with heaps of stones, where two players take it in turn to remove any number of stones from any heap until no stones remain.
+
+We'll consider the three-heap normal-play version of Nim, which works as follows:
+- At the start of the game there are three heaps of stones.
+- On his turn the player removes any positive number of stones from any single heap.
+- The first player unable to move (because no stones remain) loses.
+
+If (n1,n2,n3) indicates a Nim position consisting of heaps of size n1, n2 and n3 then there is a simple function X(n1,n2,n3) -- that you may look up or attempt to deduce for yourself -- that returns:
+*zero if, with perfect strategy, the player about to move will eventually lose; or
+*non-zero if, with perfect strategy, the player about to move will eventually win.
+
+For example X(n1,n2,n3)=0 because, no matter what the current player does, his opponent can respond with a move that leaves two heaps of equal size, at which point every move by the current player can be mirrored by his opponent until no stones remain; so the current player loses. To illustrate:
+- current player moves to (1,2,1)
+- opponent moves to (1,0,1)
+- current player moves to (0,0,1)
+- opponent moves to (0,0,0), and so wins.
+
+For how many positive integers n <= 2**30 does X(n1,n2,n3)=0 ?
+"""
+# Functions
+def nimSum(a,b,c):
+    if not (a^b^c):
+        return True
+    else:
+        return False
+
+# Constants
+num=2**30
+total=0
+# Optparse setup
+usage = "usage: %prog [OPTIONS] -n number"
+parser = OptionParser(usage=usage)
+parser.add_option("-n", "--number", action="store", type="int", dest="num", default=num, help="Finds number of losing games up to this number")
+
+(options, args) = parser.parse_args()
+
+# Solution
+s = time.time()
+
+a=1
+b=2
+c=3
+while a <= num:
+    if nimSum(a,b,c):
+        total += 1
+    a+=1
+    b+=2
+    c+=3
+
+print total,'in',time.time()-s,'secs'
