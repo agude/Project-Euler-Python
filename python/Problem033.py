761b9dd685ccdb5583cf0d68ca9f66d809d09cac
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2011-06-23 14:36:53 +0200

    Adding solution for Problem 33 in python.

---
+++ python/Problem033.py
@@ -0,0 +1,54 @@
+#  Copyright (C) 2011  Alexander Gude - alex.public.account+ProjectEulerSolutions@gmail.com
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  The most recent version of this program is avaible at:
+#  http://github.com/Falcorian/Project-Euler-Solutions
+
+import time
+from optparse import OptionParser
+from fractions import Fraction
+"""
+The fraction 49/98 is a curious fraction, as an inexperienced mathematician in attempting to simplify it may incorrectly believe that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
+
+We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+
+There are exactly four non-trivial examples of this type of fraction, less than one in value, and containing two digits in the numerator and denominator.
+
+If the product of these four fractions is given in its lowest common terms, find the value of the denominator.
+"""
+# Optparse setup
+usage = "usage: %prog [OPTIONS]"
+parser = OptionParser(usage=usage)
+
+(options, args) = parser.parse_args()
+
+# Constants
+max = 100
+
+# Solution
+s = time.time()
+tnum = 1
+tden = 1
+for i in xrange(1,10):
+    for j in xrange(1,10):
+        for k in xrange(1,10):
+            num = 10*i+j
+            den = j*10+k
+            if num < den and num*k == den*i:
+                tnum *= num
+                tden *= den
+
+print Fraction(tnum,tden),'in',time.time()-s,'secs'
