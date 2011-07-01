3f59492815c1ff5ff66b641e930c4b5c796b8899
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2011-07-01 15:48:56 +0200

    Updating Problem 5 in python to use co primes. Sped up by a factor of 4.

--- python/Problem005.py
+++ python/Problem005.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2010  Alexander Gude - alex.public.account+ProjectEulerSolutions@gmail.com
+#  Copyright (C) 2011  Alexander Gude - alex.public.account+ProjectEulerSolutions@gmail.com
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -19,31 +19,48 @@
 
 from time import time
 from optparse import OptionParser
-"""
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+""" 2520 is the smallest number that can be divided by each of the numbers from
+1 to 10 without any remainder.
 
-What is the smallest number that is evenly divisible by all of the numbers from 1 to 20?
+What is the smallest number that is evenly divisible by all of the numbers from
+1 to 20?
+
 """
 # Optparse setup
-usage = "usage: %prog [OPTIONS] -n number"
+usage = "usage: %prog [OPTIONS] -n MAX"
 parser = OptionParser(usage=usage)
-parser.add_option("-n", "--number", action="store", type="int", dest="max", default=20, help="answer must be divisible by 1,2,3,...,number")
+parser.add_option("-n", "--number", action="store", type="int", dest="MAX", default=20, help="answer must be divisible by 1,2,3,...,MAX")
 
 (options, args) = parser.parse_args()
+
+# Constants
+MAX = options.MAX
 
 # Solution
 s = time()
 
-max = options.max
+# Find number co prime in the set
+divs = [True] * MAX
+for i in range(len(divs)):
+    numi = i+1
+    for j in range(len(divs)):
+        if divs[j] and i > j:
+            numj = j+1
+            if not numi%numj:
+                divs[j] = False
 
+nums = [i+1 for i in xrange(len(divs)) if divs[i]]
+a = nums.pop()
+b = nums.pop()
+    
+# Find the smallest number divisible by all the coprime numbers
 i=0
 while True:
-    i += (max*(max-1)) # We know the number has to be divisible by max, and max-1, so we incriment by this number
-    for j in range(3,max-1): # Now we check all numbers from max-2 to 3, 2 is free if 4 is, and 1 is useless
+    i += a*b # We know the number has to be divisible by a, and b, so we incriment by this number
+    for j in nums: 
         if i%j:
             break
-        else:
-            continue
-    else: # This statement only exicutes if the for loops runs through all i%j without breaking, i.e. we have our number
-        print i,'in',time()-s,'secs'
-        exit()
+    else: # Only if above doesn't break
+        break
+
+print i,'in',time()-s,'secs'
