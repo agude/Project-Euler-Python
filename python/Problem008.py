017651e2cf2b894eb9929ee89015d90b8fb083b6
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2011-07-04 21:59:38 +0200

    Updating problem 8 in python with added command line option.

--- python/Problem008.py
+++ python/Problem008.py
@@ -17,9 +17,6 @@
 #  The most recent version of this program is avaible at:
 #  http://github.com/Falcorian/Project-Euler-Solutions
 
-from time import time
-from optparse import OptionParser
-
 """ Find the greatest product of five consecutive digits in the 1000-digit
 number:
 
@@ -46,27 +43,35 @@ number:
 
 """
 
+from time import time
+from optparse import OptionParser
+
 # Constants
 NUM = 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
 
 # Optparse setup
-usage = "usage: %prog [OPTIONS] -n NUM"
+usage = "usage: %prog [OPTIONS] -n NUM -m MAX"
 parser = OptionParser(usage=usage)
-parser.add_option("-n", "--num", action="store", type="int", dest="NUM", default=NUM, help="find the largest sum of 5 consecutive digits NUM")
+parser.add_option("-n", action="store", type="int", dest="NUM", default=NUM, help="find the largest sum of MAX consecutive digits of NUM")
+parser.add_option("-m", action="store", type="int", dest="MAX", default=5, help="find the largest sum of MAX consecutive digits of NUM")
 
 (options, args) = parser.parse_args()
 
 # Constants
 NUM = options.NUM
+MAX = options.MAX
 
 # Solution
 s = time()
 
 strnum = str(NUM)
 
-numbers = []
-for i in xrange(len(strnum)-5):
-    product = int(strnum[i])*int(strnum[i+1])*int(strnum[i+2])*int(strnum[i+3])*int(strnum[i+4])
-    numbers.append(product)
+maxnum = 0
+for i in xrange(len(strnum)-MAX):
+    product = 1
+    for j in xrange(MAX):
+        product *= int(strnum[i+j])
+    if product > maxnum:
+        maxnum = product
 
-print max(numbers),'in',time()-s,'secs'
+print maxnum,'in',time()-s,'secs'
