2bbd4b01690330080bf246b037439737d4939f0d
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2013-11-24 10:27:16 -0600

    Clean Problem001.py

---
+++ python/Problem001.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+#  Copyright (C) 2011  Alexander Gude - alex.public.account+ProjectEulerSolutions@gmail.com
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  The most recent version of this program is available at:
+#  https://github.com/agude/Project-Euler
+
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+from time import time
+
+# Only runs if executed directly
+if __name__ == '__main__':
+
+    # Optparse setup
+    from optparse import OptionParser
+    usage = "usage: %prog [OPTIONS] -n MAX"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-n", action="store", type="int", dest="MAX", default=1000, help="find the sum of the multiples of 3 or 5 below MAX")
+
+    (options, args) = parser.parse_args()
+
+    # Constants
+    MAX = options.MAX
+
+    # Solution
+    start_time = time()
+
+    total = 0
+    # We try all numbers, incrementing by 3.
+    for i in range(1, MAX):
+        if (not i % 3) or (not i % 5):
+            total += i
+
+    end_time = time() - start_time
+    print(total, 'in', end_time, 'secs')
