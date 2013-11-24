1c7e8e83a82e033f0db6b48c734302b81f959eb6
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2013-11-24 15:58:50 -0600

    Clean Problem003.py and Problem004.py

---
+++ python/Problem003.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+#  Copyright (C) 2011  Alexander Gude - alex.public.account+ProjectEulerSolutions@gmail.com
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  The most recent version of this program is available at:
+#  https://github.com/agude/Project-Euler
+
+"""
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143?
+"""
+
+# Only runs if executed directly
+if __name__ == '__main__':
+    from library import PrimeFactors
+    from optparse import OptionParser
+    from time import time
+
+    # Optparse setup
+    usage = "usage: %prog [OPTIONS] -n NUM"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-n", action="store", type="int", dest="num", default=600851475143, help="find the largest prime factor of NUM")
+
+    (options, args) = parser.parse_args()
+
+    # Constants
+    NUM = options.num
+
+    # Solution
+    s = time()
+    start_time = time()
+
+    prime_factors = PrimeFactors(NUM)
+    max_prime = prime_factors[-1]
+
+    total_time = time() - start_time
+    print(max_prime, 'in', total_time, 'secs')
