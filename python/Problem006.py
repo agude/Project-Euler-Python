fdb49d27b0e8ab70ad6514353e099a91a56853cb
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2013-11-24 20:18:08 -0600

    Clean Problem006.py

--- python/Problem006.py
+++ python/Problem006.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 #  Copyright (C) 2011  Alexander Gude - alex.public.account+ProjectEulerSolutions@gmail.com
 #
 #  This program is free software; you can redistribute it and/or modify
@@ -13,8 +15,12 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software Foundation,
 #  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  The most recent version of this program is available at:
+#  https://github.com/agude/Project-Euler
 
-""" The sum of the squares of the first ten natural numbers is:
+"""
+The sum of the squares of the first ten natural numbers is:
 1**2 + 2**2 + ... + 10**2 = 385
 
 The square of the sum of the first ten natural numbers is:
@@ -25,29 +31,33 @@ numbers and the square of the sum is 3025 - 385 = 2640.
 
 Find the difference between the sum of the squares of the first one hundred
 natural numbers and the square of the sum.
-
 """
 
-from time import time
-from optparse import OptionParser
+# Only runs if executed directly
+if __name__ == '__main__':
+    from time import time
+    from optparse import OptionParser
 
-# Optparse setup
-usage = "usage: %prog [OPTIONS] -n MAX"
-parser = OptionParser(usage=usage)
-parser.add_option("-n", action="store", type="int", dest="MAX", default=100, help="find (1**2 + 2**2 + ... + MAX**2) - (1 + 2 + ... + MAX)**2")
+    # Optparse setup
+    usage = "usage: %prog [OPTIONS] -n MAX"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-n", action="store", type="int", dest="MAX", default=100, help="find (1**2 + 2**2 + ... + MAX**2) - (1 + 2 + ... + MAX)**2")
 
-(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-# Constants
-MAX = options.MAX
+    # Constants
+    MAX = options.MAX
 
-# Solution
-s = time()
+    # Solution
+    start_time = time()
 
-x = 0
-x2 = 0
-for i in xrange(MAX+1): # We just brute force it
-    x += i
-    x2 += i*i
+    x = 0
+    x2 = 0
+    for i in range(MAX + 1): # We just brute force it
+        x += i
+        x2 += i * i
 
-print (x*x - x2),'in',time()-s,'secs'
+    result = x * x - x2
+
+    total_time = time() - start_time
+    print(result, 'in', total_time, 'secs')
