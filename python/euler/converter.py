9ed7b56b39881c3578890bcc850e2d9a8b5254b4
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2014-06-15 10:58:25 -0500

    Optimize euler.converter
    Switching to strings is faster than our previous methods, as measured by
    timeit.timeit.

--- python/euler/converter.py
+++ python/euler/converter.py
@@ -4,6 +4,8 @@ def iterable_to_int(input_tuple):
 
     For example: (1, 2, 3) -> 123
 
+    Note that it only support numbers >= 0.
+
     Args:
         input_tuple (ordered iterable): An order iterable of the digits of an
             integer in order from most to least significant digit.
@@ -14,14 +16,29 @@ def iterable_to_int(input_tuple):
     Raises:
         TypeError: If input_tuple does not support reversed().
     """
-    number = 0
-    place = 0
-    for digit in reversed(input_tuple):
-        number += (10 ** place) * digit
-        place += 1
-
-    return number
+    # Using a string is faster than looping over the tuple and adding numbers
+    str_tuple = (str(i) for i in input_tuple)
+    return int(''.join(str_tuple))
 
 
 def int_to_tuple(number):
-    pass
+    """Takes an integer and returns a tuple of the digits in order.
+
+    For example: 123 -> (1, 2, 3)
+
+    Note that it only support numbers >= 0.
+
+    Args:
+        number (int): An integer to turn into a tuple.
+
+    Returns:
+        tuple: The tuple representation of the input number.
+
+    Raises:
+        ValueError: If number is < 0.
+    """
+    if number < 0:
+        raise ValueError("Input less than 0")
+    # Using a string might not be as clean as some sort of division and modulo
+    # loop, but using the string is much faster for large numbers
+    return tuple([int(i) for i in str(number)])
