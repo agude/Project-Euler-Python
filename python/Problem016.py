28c707635634e21f02a76d46fb11611fce853abf
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2014-06-29 16:50:17 -0500

    Update Python Problem 16

---
+++ python/Problem016.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+#  Copyright (C) 2014  Alexander Gude - alex.public.account+ProjectEulerSolutions@gmail.com
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  The most recent version of this program is available at:
+#  https://github.com/agude/Project-Euler
+
+"""
+2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2**1000?
+"""
+
+# Only runs if executed directly
+if __name__ == '__main__':
+    from euler.converter import int_to_tuple
+    from optparse import OptionParser
+    from time import time
+
+    # Optparse setup
+    usage = "usage: %prog [OPTIONS] -n NUM"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-n", "--num", action="store", type="int", dest="NUM", default=2**1000, help="find the sum of the digits of NUM")
+
+    (options, args) = parser.parse_args()
+
+    # Constants
+    NUM = options.NUM
+
+    # Solution
+    start_time = time()
+
+    answer = sum(int_to_tuple(NUM))
+
+    end_time = time() - start_time
+    print(answer, 'in', end_time, 'secs')
