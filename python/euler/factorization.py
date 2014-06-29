63848b6fb0383c3150410f8a55a7efba6c8bcfe3
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2014-06-29 14:17:05 -0500

    Add proper factor functions to euler.factorization

--- python/euler/factorization.py
+++ python/euler/factorization.py
@@ -1,24 +1,29 @@
 import bisect
 import math
-import numpy
 
 
-def factors(number):
-    """ Returns a list of the factors of a number.
+def proper_factors(number):
+    """ Returns a list of the proper factors of a number.
+
+    The proper factors of a number are all numbers that divide it evenly,
+    except for itself (although 1 is included).
 
     Args:
         number (int): The number to factorize.
 
     Returns:
-        list: A sorted list of all the factors of the number.
+        list: A sorted list of all the proper factors of the number.
 
     Raises:
         TypeError: number is not convertible to a float (for sqrt).
     """
     bound = int(math.floor(math.sqrt(number)))
-    factors = []
+    # All numbers are divisible by 1
+    if number == 1:
+        return []
+    factors = [1]
     # Try all divisors up to the square root of the number, add those
-    for i in range(1, bound + 1):
+    for i in range(2, bound + 1):
         if not number % i:
             fact1 = i
             fact2 = number / i
@@ -32,8 +37,49 @@ def factors(number):
     return factors
 
 
+def factors(number):
+    """ Returns a list of the factors of a number.
+
+    The factors of a number are all numbers that divide it evenly, including
+    itself.
+
+    Args:
+        number (int): The number to factorize.
+
+    Returns:
+        list: A sorted list of all the proper factors of the number.
+
+    Raises:
+        TypeError: number is not convertible to a float (for sqrt).
+    """
+    return proper_factors(number) + [number]
+
+
+def number_of_proper_factors(number):
+    """ Returns the number of proper factors of a number.
+
+    The proper factors of a number are all numbers that divide it evenly,
+    except for itself (although 1 is included).
+
+    This is equivilent to len(proper_factors(number)).
+
+    Args:
+        number (int): The number to compute the number of factors for.
+
+    Returns:
+        int: The number of factors.
+
+    Raises:
+        TypeError: number is not convertible to a float (for sqrt).
+    """
+    return len(proper_factors(number))
+
+
 def number_of_factors(number):
     """ Returns the number of factors of a number.
+
+    The factors of a number are all numbers that divide it evenly, including
+    itself.
 
     This is equivilent to len(factors(number)).
 
