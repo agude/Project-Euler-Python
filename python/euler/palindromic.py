ebb4e682fae23d3cf14a42259e73a4e8fb245dad
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2014-07-18 19:41:51 -0500

    Support for negative numbers in euler.palindromic
    All numbers now have their absolue value taken, and this result is
    checked instead. This removes issues of having minus signs in the
    number.

--- python/euler/palindromic.py
+++ python/euler/palindromic.py
@@ -2,19 +2,23 @@ def is_palindromic(number):
     """Returns True if the number is palindromic, that is, read the same
     forward as backwards, otherwise returns False.
 
+    Negative numbers are palindromic if their absolute values are palindromic.
+
     Args:
         number (int): The number to test.
 
     Returns:
         bool: True if number is palindromic, False otherwise.
     """
-    test_number = str(number)
+    test_number = str(abs(number))
     return test_number == test_number[::-1]  # [::-1] reverses a list
 
 
 def is_binary_palindromic(number):
     """Returns True if the number is palindromic in binary, that is, read the
     same forward as backwards, otherwise returns False.
+
+    Negative numbers are palindromic if their absolute values are palindromic.
 
     Args:
         number (int): The number to test.
@@ -23,5 +27,5 @@ def is_binary_palindromic(number):
         bool: True if number's binary representation is palindromic, False
             otherwise.
     """
-    binary_string = bin(number)[2:]  # We strip the "0b" prefix
+    binary_string = bin(abs(number))[2:]  # We strip the "0b" prefix
     return binary_string == binary_string[::-1]  # [::-1] reverses a list
