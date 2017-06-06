3c203c894c2ce2cc8680296f5e6506a4d419619e
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2017-06-06 08:42:30 -0700

    Refactor python problem 022 and add tests

--- python/Problem022.py
+++ python/Problem022.py
@@ -32,6 +32,10 @@ obtain a score of 938  53 = 49714.
 What is the total of all the name scores in the file?
 """
 
+from string import ascii_lowercase
+from time import time
+
+
 NAMES = [
     "MARY",
     "PATRICIA",
@@ -5195,60 +5199,42 @@ NAMES = [
     "DORSEY",
     "DARELL",
     "BRODERICK",
-    "ALONSO"
+    "ALONSO",
 ]
 
-POINTS = {
-    'A': 1, 'a': 1,
-    'B': 2, 'b': 2,
-    'C': 3, 'c': 3,
-    'D': 4, 'd': 4,
-    'E': 5, 'e': 5,
-    'F': 6, 'f': 6,
-    'G': 7, 'g': 7,
-    'H': 8, 'h': 8,
-    'I': 9, 'i': 9,
-    'J': 10, 'j': 10,
-    'K': 11, 'k': 11,
-    'L': 12, 'l': 12,
-    'M': 13, 'm': 13,
-    'N': 14, 'n': 14,
-    'O': 15, 'o': 15,
-    'P': 16, 'p': 16,
-    'Q': 17, 'q': 17,
-    'R': 18, 'r': 18,
-    'S': 19, 's': 19,
-    'T': 20, 't': 20,
-    'U': 21, 'u': 21,
-    'V': 22, 'v': 22,
-    'W': 23, 'w': 23,
-    'X': 24, 'x': 24,
-    'Y': 25, 'y': 25,
-    'Z': 26, 'z': 26
-}
+# The points for each letter is the letter's position in the alphabet, so we
+# create a mapping of letter to position to make the lookup fast.
+POINTS = {}
+for i, char in enumerate(ascii_lowercase):
+    score = i + 1
+    POINTS[char] = score
 
-# Only runs if executed directly
-if __name__ == '__main__':
-    from time import time
-    from optparse import OptionParser
 
+def problem_022(names=NAMES):
     # Solution
     start_time = time()
 
     # We are instructed to sort the names in alphabetical order
-    NAMES.sort()
+    names.sort()
 
     # We loop through the names and compute their score by summing the position
     # of their letters in the alphabet (A=1, B=2, etc), we then multiply this
     # by their position in the list (indexed from 1 instead of 0).
     answer = 0
-    for index in range(len(NAMES)):
-        name = NAMES[index]
-        position = index + 1
+    for index, name in enumerate(names):
+        name = name.lower()
         score = 0
         for letter in name:
             score += POINTS[letter]
+        position = index + 1
         answer += position * score
 
     end_time = time() - start_time
     print(answer, 'in', end_time, 'secs')
+    return answer
+
+
+# Only runs if executed directly
+if __name__ == '__main__':
+
+    problem_022(NAMES)
---
+++ python/tests/problems/test_problem_022.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import pytest
+
+from Problem022 import problem_022
+
+
+def test_problem_022():
+    assert problem_022() == 871198282
