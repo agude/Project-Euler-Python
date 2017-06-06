e830242a0c3d351bd1faa672b9b378fb561f1dc9
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2017-06-06 09:25:18 -0700

    Fix UTF8 issue with Problem020.py

--- python/Problem020.py
+++ python/Problem020.py
@@ -20,7 +20,7 @@
 #  https://github.com/agude/Project-Euler
 
 """
-n! means n * (n − 1) * ... * 3 * 2 * 1
+n! means n * (n - 1) * ... * 3 * 2 * 1
 
 For example, 10! = 10 * 9 * ... * 3 * 2 * 1 = 3628800, and the sum of the
 digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
