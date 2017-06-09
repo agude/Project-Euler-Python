b5c2998c7f068d23eac76f13f9676def42df5fd3
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2017-06-08 21:50:00 -0400

    Refactor python problem 051 and add tests

---
+++ python/tests/problems/test_problem_051.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import pytest
+
+from Problem051 import problem_051
+
+
+@pytest.mark.skip(reason="Problem 51 takes about 30 seconds to complete.")
+def test_problem_051():
+    assert problem_051() == 120383
