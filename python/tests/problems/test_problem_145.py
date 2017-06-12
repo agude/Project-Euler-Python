0c7de29a7fbecf6767d0cdaef25b3a000722a75b
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2017-06-11 21:46:30 -0700

    Fix timing note for Problem 145 test

--- python/tests/problems/test_problem_145.py
+++ python/tests/problems/test_problem_145.py
@@ -9,6 +9,6 @@ def test_problem_145_fast():
     assert problem_145(1000) == 120
 
 
-@pytest.mark.skip(reason="Problem 145 takes over 10 minutes to complete.")
+@pytest.mark.skip(reason="Problem 145 takes over 40 minutes to complete.")
 def test_problem_145():
     assert problem_145() == 608720
