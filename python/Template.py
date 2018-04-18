575cdddf339ec6f6511518b04e1990b4993e0a12
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2018-04-17 19:58:12 -0700

    Update template for Python

--- python/Template.py
+++ python/Template.py
@@ -1,4 +1,6 @@
-#  Copyright (C) 2013  Alexander Gude - alex.public.account+ProjectEulerSolutions@gmail.com
+#!/usr/bin/env python3
+
+#  Copyright (C) 2018  Alexander Gude - alex.public.account+ProjectEulerSolutions@gmail.com
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -13,25 +15,44 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software Foundation,
 #  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  The most recent version of this program is available at:
+#  https://github.com/agude/Project-Euler
 
-from time import time
-from optparse import OptionParser
 """
 """
-# Optparse setup
-usage = "usage: %prog [OPTIONS]"
-parser = OptionParser(usage=usage)
-parser.add_option("-n", "--num", action="store", type="int", dest="num", default=0, help="")
 
-(options, args) = parser.parse_args()
-
-# Classes
 
 # Functions
+def problem_001(max_num=1000):
+    from time import time
 
-# Constants
+    # Solution
+    start_time = time()
 
-# Solution
-s = time()
+    # Generate the sets of numbers
 
-print 'in',time()-s,'secs'
+    # SOLUTION
+    total = 1
+
+    end_time = time() - start_time
+    print(total, 'in', end_time, 'secs')
+
+    return total
+
+
+# Only runs if executed directly
+if __name__ == '__main__':
+    from optparse import OptionParser
+
+    # Optparse setup
+    usage = "usage: %prog [OPTIONS]"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-n", "--num", action="store", type="int", dest="num", default=0, help="")
+
+    (options, args) = parser.parse_args()
+
+    # Constants
+    MAX = options.MAX
+
+    problem_001(MAX)
