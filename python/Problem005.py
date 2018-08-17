58a510c0e66a36db282de6c32ed1fa4d87820ebc
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2018-08-17 06:52:06 -0700

    Add in factor pruning to Problem 005
    This fixes #3.

--- python/Problem005.py
+++ python/Problem005.py
@@ -27,9 +27,8 @@ What is the smallest number that is evenly divisible by all of the numbers from
 1 to 20?
 """
 
-import numpy as np
-from typing import List
-from euler.primes import are_coprime
+from typing import List, Set
+from euler.factorization import proper_factors
 
 
 def problem_005(max_num: int = 20) -> int:
@@ -38,12 +37,23 @@ def problem_005(max_num: int = 20) -> int:
     # Solution
     start_time: float = time()
 
+    # We can remove any numbers that are factors of other numbers in the list.
+    # That is, if 2*N divides a number, then N does also.
+    seen_divisors: Set[int] = set()
+    final_divisors: List[int] = []
+    for number in range(max_num, 1, -1):
+        if number not in seen_divisors:
+            final_divisors.append(number)
+
+        current_factors: Set[int] = set(proper_factors(number))
+        seen_divisors = seen_divisors.union(current_factors)
+
     # We know the number has to be divisible by the two largest numbers, so we
     # increment by the product of them.
-    final_divisors: List[int] = list(range(1, max_num+1))
-    a: int = final_divisors.pop()
-    b: int = final_divisors.pop()
-    increment: int = a * b
+    final_divisors.sort()
+    largest: int = final_divisors.pop()
+    second_largest: int = final_divisors.pop()
+    increment: int = largest * second_largest
 
     # Find the smallest number divisible by all the coprime numbers
     test_number: int = 0
