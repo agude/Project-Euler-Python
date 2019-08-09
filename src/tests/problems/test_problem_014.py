b6f08ac323b5b4b01afa2a0a76c3316874ce05b2
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2019-08-08 19:30:07 -0700

    Add some fast tests for Problem 14

--- src/tests/problems/test_problem_014.py
+++ src/tests/problems/test_problem_014.py
@@ -5,5 +5,9 @@ import pytest
 from Problem014 import problem_014
 
 
+def test_problem_014_fast():
+    assert problem_014(20) == 19
+    assert problem_014(1000) == 871
+
 def test_problem_014():
-    assert problem_014() == 837799
+    assert problem_014() == 837_799
